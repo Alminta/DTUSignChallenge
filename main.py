d4b1c4b97b9e624483b95b0158f70ae1002754e9
Alminta <almintars@gmail.com>
2018-10-29 23:05:32 +0100

    Yolosapiensv3

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#%%
 #%matplotlib inline
 import matplotlib
 import numpy as np
@@ -9,6 +10,7 @@ import time
 import cv2
 import random
 
+
 from os import listdir
 
 #from IPython.display import clear_output
@@ -19,7 +21,28 @@ from skimage.transform import resize
 import colorsys
 from matplotlib.pyplot import figure
 
+def hlsThres(hls,thres):
+    hls=np.abs(hls-np.ones(hls.shape)*90)
+    hls[hls[...,0]<thres]=0
+    #hls[np.logical_and(hls[...,0]>thres,hls[...,0]<180-thres)]=100
+    return hls
 
 
-listdir('Data')
+a= listdir('Data')
+print(a)
+
+
+img = cv2.imread('Data/DTUSigns057.jpg')[...,::-1]
+
+
+
+hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS).astype(float)
+
+hls = hlsThres(hls,7)
+
+
+
+figure(figsize=(16, 16), dpi=80)
+plt.imshow(hls[...,0],cmap='Greys')
+plt.show()
 
